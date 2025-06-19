89946cf6aa0e782806c904f2a0956b76e7f8db5c
KHUBAIB <khubaibnaeem42@gmail.com>
2025-06-19 13:39:56 +0500

    UMS

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace UniversityManagementSystem
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("HELLO WORLD!");
+        }
+    }
+}
